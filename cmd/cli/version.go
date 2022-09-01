bc2544ca4cc4f0c1f8f8b00255132e17522f6e7c
Austin Valle <austinvalle@gmail.com>
2022-08-31 22:31:01 -0400

    added version info

--- cmd/cli/version.go
+++ cmd/cli/version.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+
 	"github.com/austinvalle/discord-guild-probe/pkg/discord"
 )
 
 type VersionCmd struct{}
 
+var commit = "local"
+
+func init() {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, s := range info.Settings {
+			if s.Key == "vcs.revision" {
+				commit = s.Value
+			}
+		}
+	}
+}
+
 func (cmd *VersionCmd) Run(ctx *Context) error {
+	fmt.Printf("dgp - commit: %s - %s/%s\n", commit, runtime.GOOS, runtime.GOARCH)
+
 	discord.Test("Hello World")
 	return nil
 }
